ae35e37809bd7a44edf017d25d7773314bde9adf
EvasiveXkiller <58895502+EvasiveXkiller@users.noreply.github.com>
2021-02-02 13:10:18 +0800

    fixed db password

--- dbconn.php
+++ dbconn.php
@@ -2,7 +2,7 @@
 
 $servaddress = "localhost:3306";
 $username = "root";
-$password = "123456";
+$password = "12345";
 $remotedbname  = "almuni";
 
 $conn = mysqli_connect($servaddress,$username,$password,$remotedbname);
