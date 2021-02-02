c2b8642390dc0e8357699ebe15fc82f027d5a8e8
OptimalLego <77846524+OptimalLego@users.noreply.github.com>
2021-02-02 11:09:34 +0800

    Reformatted homepage

--- dbconn.php
+++ dbconn.php
@@ -2,7 +2,7 @@
 
 $servaddress = "localhost:3306";
 $username = "root";
-$password = "12345";
+$password = "123456";
 $remotedbname  = "almuni";
 
 $conn = mysqli_connect($servaddress,$username,$password,$remotedbname);
