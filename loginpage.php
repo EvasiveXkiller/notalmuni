afde56420d7a9456b677985649bfa87e4033934c
OptimalLego <77846524+OptimalLego@users.noreply.github.com>
2021-02-04 18:42:37 +0800

    Redesigned login page for user and admin

--- loginpage.php
+++ loginpage.php
@@ -19,25 +19,30 @@
         <div class="formbox">
             <form action="loginprocessor.php" method="POST">
                 <div class="form">
-                    <img src="assets/EmptyProfilePicture.png" class="profilepicture">
-                    <label for="username">Username:</label>
-                    <input type="text" id="username" name="username" style="height:25px; display:flex;" required><br>
+                    <img src="assets/spolight.jpg" class="picture"><br>
+                    <div class="buttons">
+                    <a href="adminloginpage.html" class="userloginbutton">ADMIN LOGIN</a><br><br>
+                    </div>
+                    <label for="username"></label>
+                    <input type="text" id="username" name="username" style="height:25px; display:flex; border-radius:40px;" placeholder="Username"required><br>
 
-                    <label for="pword">Password:</label>
-                    <input type="password" id="pword" name="pword" style="height:25px;" required><br>
+                    <label for="pword"></label>
+                    <input type="password" id="pword" name="pword" placeholder="Password"style="height:25px;border:1px; border-radius:40px;" required>
 
+                    <p class="forgotpassword"><a href="resetpasswordpage.html" style="text-decoration: none; color:#40736E">Forgot password?</a></p>
                 </div>
                 <div class="buttons">
-                    <input type="submit" class="loginbutton" style="height:25px;" value="Login" name="login"></input><br>
+                    <input type="submit" class="loginbutton" style="height:30px;" value="Login" name="login"></input><br>
                 </div>
-                <p style="text-align:center"> Not a user? <a href="registerpage.php">Register here!</a></p>
+                <p style="text-align:center"> Not a user? <a href="registerpage.php">Register here!</a></p><br>
+                <p style="font-size:10px; text-align:center;"> Wedge &copy All Rights Reserved</p>
             </form>
         </div>
         <script src="clock.js"></script>
     </body>
 
     </html>
-    <?php
+    <!-- <?php
         if (isset($_GET["state"])) {
             if($_GET["state"] == "err") {
         ?>
@@ -53,4 +58,4 @@
         <?php
         }
         }
-        ?>
+        ?> -->
