4b17a13b4f2f87e2e6772870bbaab6fdbb36c538
EvasiveXkiller <58895502+EvasiveXkiller@users.noreply.github.com>
2021-02-05 10:26:11 +0800

    admindashboard logic working

---
+++ admin/dashboardprocessor.php
@@ -0,0 +1,30 @@
+<?php
+
+include("../dbconn.php");
+
+if (!isset($_GET["origin"])) {
+    echo "illegal origin";
+}
+$send = array();
+
+$selectsql = "SELECT * FROM `users`";
+
+$output = mysqli_query($conn, $selectsql);
+$send["contactscount"] = mysqli_num_rows($output);
+
+$selectsql = "SELECT * FROM `users`";
+
+$output = mysqli_query($conn, $selectsql);
+$send["userscount"] = mysqli_num_rows($output);
+
+$selectsql = "SELECT * FROM `users` where status_='pending'";
+
+$output = mysqli_query($conn, $selectsql);
+$send["pendingscount"] = mysqli_num_rows($output);
+
+$selectsql = "SELECT * FROM `admin`";
+
+$output = mysqli_query($conn, $selectsql);
+$send["admincount"] = mysqli_num_rows($output);
+
+echo json_encode($send);
