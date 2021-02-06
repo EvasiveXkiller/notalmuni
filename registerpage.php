d13126b450d0fe11cce76f6a2cc7a70920991b5e
EvasiveXkiller <58895502+EvasiveXkiller@users.noreply.github.com>
2021-02-06 21:55:16 +0800

    major overhaul in register page

--- registerpage.php
+++ registerpage.php
@@ -1,83 +1,166 @@
 <!DOCTYPE html>
 <html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Register Page</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="./css/registerpage.css" />
+	</head>
 
-<head>
-	<meta charset="utf-8" />
-	<title>Register Page</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="stylesheet" href="./css/loginpage.css" />
-</head>
-
-<body class="body">
-	<div class="topbar">
-		<p>
-			<span class="alumnicrm">ALUMNI CRM</span><b class="welcometoalumni">Welcome to Almuni!</b><span id="clock"
-				class="clock"></span>
-		</p>
-	</div>
-
-	<div class="registerformboxtable">
-		<table style="border-spacing:15px; border-collapse: separate;">
+	<body>
+		<div class="topbar">
+			<p>
+				<span class="alumnicrm">ALUMNI CRM</span
+				><b class="welcometoalumni">Welcome to Almuni!</b
+				><span id="clock" class="clock"></span>
+			</p>
+		</div>
 		<form action="registerprocessor.php" method="POST">
-				<tr>
-				<td colspan="4">
-				<div class="form">
-				<img src="assets/EmptyProfilePicture.png" class="picture" style="max-width:30%;" />
-				</td>
-				</tr>
-				<tr>
-				<td><label for="name">Name: *</label></td>
-				<td><input type="text" id="name" name="name" style="height: 25px" required /><br /></td>
-				<td><label for="pword">Password: *</label></td>
-				<td><input type="password" id="pword" name="pword" style="height: 25px" required /><br /></td>
-				</tr>
-				<tr>
-				<td><label for="email">Email: *</label></td>
-				<td><input type="email" id="email" name="email" style="height: 25px" placeholder="abc@gmail.com"
-					required /><br /></td>
-				<td><label for ="pword2">Confirm Password: *</label></td>
-				<td><input type="password" id="pword2" name="pword2" style="height:25px" required></td>
-				</tr>
-				<tr>
-				<td><label for="phone">Phone Number: *</label></td>
-				<td><input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-					placeholder="012-345-6789" required /><br /></td>
-				<td><label for="IC">Identification Number: *</label></td>
-				<td><input type="number" id="IC" name="IC" maxlength="12" placeholder="12 digit IC" required /><br /></td>
-				</tr>
-				<tr>
-				<td><label for="location">Address: </label></td>
-				<td><textarea id="location" name="location" rows="2" style="height: 50px"
-					placeholder="Name, street number, street name, region, postal code and town/city, state."></textarea><br /></td>
-				<td><div class="radio">
-					Gender: *
-					</td>
-					<td><label for="male">Male</label>
-					<input type="radio" id="male" name="gender" value="male" required /><br />
-					<label for="female">Female</label>
-					<input type="radio" id="female" name="gender" value="female" /><br />
-					<label for="other">Other</label>
-					<input type="radio" id="other" name="gender" value="other" /><br /><br />
-					</td>
+			<div class="centerbox">
+				<div class="boxtitle">Create your Almuni Account</div>
+				<div class="registerformboxtable">
+					<div class="col">
+						<div class="form__group field">
+							<input
+								type="input"
+								class="form__field"
+								placeholder="Name"
+								name="name"
+								id="name"
+								required
+							/>
+							<label for="name" class="form__label">Name</label>
+						</div>
+						<div class="form__group field">
+							<input
+								type="email"
+								class="form__field"
+								placeholder="Email"
+								name="name"
+								id="email"
+								required
+							/>
+							<label for="email" class="form__label">Email</label>
+						</div>
+						<div class="form__group field">
+							<input
+								type="tel"
+								class="form__field"
+								placeholder="Phone Number"
+								name="phone"
+								id="phone"
+								pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+								required
+							/>
+							<label for="phone" class="form__label"
+								>Phone Number</label
+							>
+						</div>
+						<div class="form__group field">
+							<input
+								type="text"
+								class="form__field"
+								placeholder="Address"
+								name="location"
+								id="address"
+							/>
+							<label for="address" class="form__label"
+								>Address</label
+							>
+						</div>
+					</div>
+					<div class="col">
+						<div class="form__group field">
+							<input
+								type="password"
+								class="form__field"
+								placeholder="Passowrd"
+								name="pword"
+								id="pword"
+								required
+							/>
+							<label for="pword" class="form__label"
+								>Password</label
+							>
+						</div>
+						<div class="form__group field">
+							<input
+								type="password"
+								class="form__field"
+								placeholder="Passowrd"
+								name="pword2"
+								id="pword2"
+								required
+							/>
+							<label for="pword2" class="form__label"
+								>Comfirm Password</label
+							>
+						</div>
+						<div class="form__group field">
+							<input
+								type="number"
+								class="form__field"
+								placeholder="Identity Number"
+								name="IC"
+								maxlength="12"
+								id="IC"
+							/>
+							<label for="IC" class="form__label"
+								>Identity Number</label
+							>
+						</div>
+
+						<div
+							class="form__group field"
+							style="padding-bottom: 15px"
+						>
+							Gender:
+							<div class="radiowrapper">
+								<input
+									type="radio"
+									id="male"
+									name="gender"
+									value="male"
+									checked
+									required
+								/>
+								<label for="male">Male</label>
+							</div>
+							<div class="radiowrapper">
+								<input
+									type="radio"
+									id="female"
+									name="gender"
+									value="female"
+								/>
+								<label for="female">Female</label>
+							</div>
+							<div class="radiowrapper">
+								<input
+									type="radio"
+									id="other"
+									name="gender"
+									value="other"
+								/>
+								<label for="other">Other</label>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="footerboxsubmit">
+					<button type="submit" value="Register" name="Register">Register</button><br />
+				</div>
+				<div class="footerboxsignin">
+					<button onclick="redirect()">Sign In Instead</button><br />
 				</div>
 			</div>
-			<tr>
-				<td colspan="4">
-			<div class="buttons">
-				<input type="submit" class="loginbutton" style="height: 25px" value="Register" name="Register" />
-				<p style="
-							font-size: 12px;
-							color: rgba(255, 0, 0, 0.726);
-							font-weight: bold;
-						">
-					* -Required information
-				</p>
-				</td>
-			</div>
-			</tr>
 		</form>
-	</div>
-	<script src="clock.js"></script>
-</body>
-
+		<script src="clock.js"></script>
+		<script>
+			function redirect() {
+				event.preventDefault()
+				window.location.href = "./loginpage.php"
+			}
+		</script>
+	</body>
 </html>
