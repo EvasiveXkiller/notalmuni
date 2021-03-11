e9774952614ffab4131fe733d5fd79d03ce97971
OptimalLego <77846524+OptimalLego@users.noreply.github.com>
2021-03-11 09:40:01 +0800

    loginpage.php added php code to check for empty text boxes dbconn port also changed

--- loginpage.php
+++ loginpage.php
@@ -1,3 +1,55 @@
+<?php
+    include("dbconn.php");
+    include("processor.php");
+
+    session_start();
+
+    if(!isset($_POST["login"])) {
+        //header("location:./loginpage.php");
+        echo "illegal origin";
+    }
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = safe_converter($_POST["username"]);
+        $password = safe_converter($_POST["pword"]);
+
+        $sqlstmt = "SELECT * FROM `users` WHERE username='" . $username. "'";
+
+        $result = mysqli_query($conn, $sqlstmt);
+        $resultCheck = mysqli_num_rows($result);
+        if($resultCheck != 1) {
+           echo "Error";
+        }
+        $output = mysqli_fetch_assoc($result);
+        if($output["status_"] === "pending") {
+            header("location:./loginpage.php?state=pending");
+            exit(0);
+        }
+        if(password_verify($password, $output["user_password"])) {
+            $_SESSION["ID"] = $output["user_ID"];
+            header("location:./user/dashboard.php");
+        } else {
+            header("location:./loginpage.php?state=err");
+        }
+    }
+
+    $name = $password ="";//this is needed to avoid undefined error
+    $errors = array('name' => "", 'password' => "");//associative array
+        
+    if (isset($_POST['login'])){
+        if (empty($_POST['name'])){
+            $errors['name'] = "Username is required";
+        } 
+        else {
+            $name = $_POST['name'];
+        }
+        if (empty($_POST['password'])){
+            $errors['password'] = "Password is required";
+        }
+        else {
+            $password = $_POST['password'];
+        }
+    }
+    ?>
 <!DOCTYPE html>
 <html>
 
@@ -14,31 +66,39 @@
 
     </div>
     <div class="formbox">
-        <form action="loginprocessor.php" method="POST">
+        <form action="loginpage.php" method="POST">
             <div class="form">
                 <img src="assets/collegestudent4.jpg" class="picture"><br>
                 <label class="switch">
                     <input type="checkbox" id="togBtn">
                     <div class="slider round"></div>
                 </label>
+
                 <div class="form__group field">
-                    <input type="input" class="form__field" placeholder="Name" name="username" id='name' required />
+                    <input type="input" class="form__field" placeholder="Name" name="username" id='name' />
                     <label for="name" class="form__label">Name</label>
+                    <div style="color:red;"><?php echo $errors['name'] ?></div>
                 </div>
+
                 <div class="form__group field">
-                    <input type="password" class="form__field" placeholder="Password" name="pword" id='pword' required />
+                    <input type="password" class="form__field" placeholder="Password" name="pword" id='pword'  />
                     <label for="pword" class="form__label">Password</label>
+                    <div style="color:red;"><?php echo $errors['password'] ?></div>
                 </div>
+
                 <p class="forgotpassword"><a href="resetpasswordpage.php" style="text-decoration: none; color:#40736E">Forgot password?</a></p>
             </div>
+
             <div class="buttons">
                 <input type="submit" class="loginbutton" style="height:30px;" value="Login" name="login"></input><br>
             </div>
+
             <br>
             <p style="text-align:center"> Not a user? <a href="registerpage.php" style="color: unset; text-decoration: underline;">Register here!</a></p><br>
             <p style="font-size:10px; text-align:center;"> <i>WEDGE</i> &copy All Rights Reserved</p>
         </form>
     </div>
+
     <script src="clock.js"></script>
     <script>
         document.getElementById("togBtn").addEventListener("click", () => {
